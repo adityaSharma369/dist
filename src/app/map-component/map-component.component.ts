cf1217ab5a6fed3244d19f7c4199139dfdc82092
anand <anand@tericsoft.com>
2020-09-17 14:50:05 +0530

    integrated with rxjs and angular

--- src/app/map-component/map-component.component.ts
+++ src/app/map-component/map-component.component.ts
@@ -1,35 +1,58 @@
-import {AfterViewInit, Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import * as $ from 'jquery';
-import { ScrollingModule } from '@angular/cdk/scrolling';
-import { retry } from 'rxjs/operators';
 import {HttpClient} from '@angular/common/http'
 import {CommonService} from '../shared/services/common.service';
-import { parse } from 'querystring';
+import {Observable, fromEvent} from "rxjs";
+
+
+interface Location {
+  x: number,
+  y: number
+}
+
 // import * as data from './../../location.json';
 @Component({
   selector: 'ts-map-component',
   templateUrl: './map-component.component.html',
-  styleUrls: ['./map-component.component.scss']
+  styleUrls: ['./map-component.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class MapComponentComponent implements OnInit,AfterViewInit {
-  polygon:any[] = [];
-  asset_coord:any[] = [];
-  access_token:any;
-  
 
-  @ViewChild("localVideo",{ static: true }) localVideo:ElementRef;
-  @ViewChild("remoteVideo",{ static: true }) remoteVideo:ElementRef;
-  @ViewChild("remoteVideo1",{ static: true }) remoteVideo1:ElementRef;
-  @ViewChild("remoteVideo2",{ static: true }) remoteVideo2:ElementRef;
-  @ViewChild("remoteVideo3",{ static: true }) remoteVideo3:ElementRef;
-  @ViewChild("remoteVideo4",{ static: true }) remoteVideo4:ElementRef;
-  @ViewChild("remoteVideo5",{ static: true }) remoteVideo5:ElementRef;
+export class MapComponentComponent implements OnInit, AfterViewInit {
+  polygon: any[] = [];
+  asset_coord: any[] = [];
+  access_token: any;
+  character_animation_time = 50;
 
+  characters = []
+
+  my_location: any = {
+    x: 50,
+    y: 100
+  };
+
+  character_width = 60;
+  character_height = 98;
+
+  map_width = 2800;
+  map_height = 2800;
+
+  step_count = 50;
+
+
+  @ViewChild("localVideo", {static: true}) localVideo: ElementRef;
+  @ViewChild("remoteVideo", {static: true}) remoteVideo: ElementRef;
+  @ViewChild("remoteVideo1", {static: true}) remoteVideo1: ElementRef;
+  @ViewChild("remoteVideo2", {static: true}) remoteVideo2: ElementRef;
+  @ViewChild("remoteVideo3", {static: true}) remoteVideo3: ElementRef;
+  @ViewChild("remoteVideo4", {static: true}) remoteVideo4: ElementRef;
+  @ViewChild("remoteVideo5", {static: true}) remoteVideo5: ElementRef;
 
   // cornersX:any[] = []
   // cornersY:any[] = []
-  constructor(private httpClient:HttpClient,private common:CommonService) {
+  constructor(private httpClient: HttpClient, private common: CommonService) {
   }
+
   // ngAfterViewInit() {
   //   this.restrictive = this.position_restrictive_items()
   //   console.log(this.restrictive);
@@ -43,112 +66,179 @@ export class MapComponentComponent implements OnInit,AfterViewInit {
   //   for (let index = 0; index < restrictive.length; index++) {
   //     const element = restrictive[index];
   //     new_restricitve.push({left:left+element[0]/2,top:top+element[1]/2})
-      
+
   //   }
   //   return new_restricitve;
 
   // }
 
-  checkInside(left,top){
-    console.log(left,top,'yo')
-    // console.log(this.polygon,'in checkInside')
-    const x = left; 
-    const y  = top;
+  isPointInPolygon(left, top) {
+
+    const x = left;
+    const y = top;
 
     for (let index = 0; index < this.polygon.length; index++) {
       const element = this.polygon[index];
       let inside = false;
       for (var i = 0, j = element.length - 1; i < element.length; j = i++) {
-          let xi = element[i][0], yi = element[i][1];
-          let xj = element[j][0], yj = element[j][1];
-          
-          const intersect = ((yi > y) !== (yj > y))
-              && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
-          if (intersect) inside = !inside;
+        let xi = element[i][0], yi = element[i][1];
+        let xj = element[j][0], yj = element[j][1];
+
+        const intersect = ((yi > y) !== (yj > y))
+          && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
       }
-      if(inside){
+      if (inside) {
         return inside;
       }
     }
     return false;
   }
 
-
-  ngOnInit() {
-    this.httpClient.get('assets/location4.json').subscribe((data:any)=>{
-
+  render_assets() {
+    this.httpClient.get('assets/location4.json').subscribe((data: any) => {
       for (let index = 0; index < data.length; index++) {
         const element = data[index];
         let p = element['restricted_polygon']
         delete element['restricted_polygon']
         this.asset_coord.push(element)
-        if(p.length > 0){
+        if (p.length > 0) {
           this.polygon.push(p)
         }
       }
-      console.log(this.asset_coord);
-      console.log(this.polygon)
-      // console.log(data)
-      // this.location = data;
-      // // console.log(data);
-      // // this.location = data['Tree']
-      // // Object.keys(data).forEach(element => {
-      // //   data[element].forEach(val => {
-      // //     this.location.push(val)
-      // //   });
-      // //   // this.location.push(data[element])
-      // //   // console.log(element)
-      // // });
-      // console.log(this.location)
     })
-    $(document).on('keydown', (e: any) => {
-      const key_code = e.which || e.keyCode;
-      const current_left = $('.my-character').css('left');
-      const current_top = $('.my-character').css('top');
-      const current_width = $('.my-character').css('width');
-      const current_height = $('.my-character').css('height');
-      const character_animation_time = 50;
+  }
 
-      const left = parseFloat(current_left.replace('px', ''));
-      const top = parseFloat(current_top.replace('px', ''));
-      const width = parseFloat(current_width.replace('px', ''));
-      const height = parseFloat(current_height.replace('px,',''));
-      console.log(width,height,'right');
-      // let element1 = document.getElementsByClassName('')[0];
+  keydown_handler(e) {
+    e.preventDefault();
 
-      // console.log(element.getBoundingClientRect(),'e');
-      switch (key_code) {
-        case 37: // left
-            if(!this.checkInside(left-20,top) && !(this.checkInside(left-20,top+height))){
-              $('.my-character').dequeue().animate({left: '-=20px'}, character_animation_time);
-            }
-          break;
-        case 38: // up
-          // console.log(rect,'before')
-            if(!this.checkInside(left,top-20) && !this.checkInside(left+width,top-20) && top-20 > 0){
-              $('.my-character').dequeue().animate({top: '-=20px'}, character_animation_time);
-            }
-          break;
-        case 39: // right
-          if(!this.checkInside(left+width+20,top) && !this.checkInside(left+width+20,top+height)){
-            $('.my-character').dequeue().animate({left: '+=20px'}, character_animation_time);
-          }
-          break;
-        case 40: // down
-            if(!this.checkInside(left,top+height+20) && !this.checkInside(left+width,top+height+20)){
-              $('.my-character').dequeue().animate({top: '+=20px'}, character_animation_time);
-            }
-          break;
-        default:
-          return; // exit this handler for other keys
+    const key_code = e.which || e.keyCode;
+    let possible_conflicts = [];
+    let is_within_map_boundary = false;
+
+    let proposed_position: any = {};
+
+    switch (key_code) {
+      case 37: // left
+
+        proposed_position = {
+          x: this.my_location.x - this.step_count,
+          y: this.my_location.y
+        }
+
+        possible_conflicts.push({
+          x: this.my_location.x - this.step_count,
+          y: this.my_location.y
+        });
+
+        possible_conflicts.push({
+          x: this.my_location.x - this.step_count,
+          y: this.my_location.y + this.character_height
+        });
+
+        is_within_map_boundary = this.my_location.x - this.step_count > 0
+
+        break;
+      case 38: // up
+
+        proposed_position = {
+          x: this.my_location.x,
+          y: this.my_location.y - this.step_count
+        }
+
+        possible_conflicts.push({
+          x: this.my_location.x,
+          y: this.my_location.y - this.step_count
+        });
+
+        possible_conflicts.push({
+          x: this.my_location.x + this.character_width,
+          y: this.my_location.y - this.step_count
+        });
+
+        is_within_map_boundary = this.my_location.y - this.step_count > 0
+
+        break;
+      case 39: // right
+
+        proposed_position = {
+          x: this.my_location.x + this.step_count,
+          y: this.my_location.y
+        }
+
+        possible_conflicts.push({
+          x: this.my_location.x + this.character_width + this.step_count,
+          y: this.my_location.y
+        });
+
+        possible_conflicts.push({
+          x: this.my_location.x + this.character_width + this.step_count,
+          y: this.my_location.y + this.character_height
+        });
+
+        is_within_map_boundary = this.my_location.x + this.character_width + this.step_count < this.map_width;
+
+        break;
+      case 40: // down
+
+        proposed_position = {
+          x: this.my_location.x,
+          y: this.my_location.y + this.step_count
+        }
+
+        possible_conflicts.push({
+          x: this.my_location.x,
+          y: this.my_location.y + this.character_height + this.step_count
+        });
+
+        possible_conflicts.push({
+          x: this.my_location.x + this.character_width,
+          y: this.my_location.y + this.character_height + this.step_count
+        });
+
+        is_within_map_boundary = this.my_location.y + this.character_height + this.step_count < this.map_height;
+
+        break;
+      default:
+        return; // exit this handler for other keys
+    }
+
+    if (is_within_map_boundary) {
+
+      let is_proposed_position_valid = true;
+      possible_conflicts.forEach((position) => {
+        if (this.isPointInPolygon(position.x, position.y)) {
+          is_proposed_position_valid = false
+          return;
+        }
+      });
+
+      if (is_proposed_position_valid) {
+        $('.character').dequeue().animate({
+          left: proposed_position.x + 'px',
+          top: proposed_position.y + 'px'
+        }, this.character_animation_time);
+
+        console.log(proposed_position, "proposed")
+
+        this.my_location = proposed_position;
+
       }
-      e.preventDefault();
-    });
+    }
+  }
+
+  ngOnInit() {
+
+    this.render_assets();
+
+    fromEvent(document.body, 'keydown').subscribe(this.keydown_handler.bind(this));
+
+    // $(document).on('keydown', this.keydown_handler);
     this.twilioToken();
 
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.common.twilio.localVideo = this.localVideo;
     this.common.twilio.r.push(this.remoteVideo);
     this.common.twilio.r.push(this.remoteVideo1);
@@ -157,10 +247,45 @@ export class MapComponentComponent implements OnInit,AfterViewInit {
     this.common.twilio.r.push(this.remoteVideo4);
     this.common.twilio.r.push(this.remoteVideo5);
 
-
+    this.spawn_character(this.my_location)
   }
 
-  twilioToken(){
+  spawn_character(location: Location) {
+    console.log("mylocation inside spawsn", location)
+    let character = {
+      location: location,
+      icon: "assets/images/map_assets/Asset1.png",
+      id: Math.random()
+    }
+    this.characters.push(character);
+    // this.update_character_position("", location, 0);
+    this.render_characters(this.characters, () => {
+      // all characters rendered. update location
+      // this.update_character_position(character.id, location);
+    });
+  }
+
+  render_characters(characters, cb = null) {
+    const jquery_character_wrapper = $('.character-wrapper');
+    jquery_character_wrapper.html("");
+    characters.forEach((character) => {
+      jquery_character_wrapper.append('<div style="top:' + character.location.y + 'px;left:' + character.location.x + 'px" id="' + character.id + '" class="character"> <img src="' + character.icon + '"  > </div>'
+      );
+    })
+    if (cb) {
+      cb();
+    }
+  }
+
+  update_character_position(char_id, location: Location, animate = this.character_animation_time) {
+    console.log(location, "spawn loction")
+    $('.character#' + char_id).dequeue().animate({
+      left: location.x + 'px',
+      top: location.y + 'px'
+    }, animate);
+  }
+
+  twilioToken() {
     console.log('hello')
     let storage = JSON.parse(localStorage.getItem('token') || '{}');
     let date = Date.now();
@@ -168,33 +293,34 @@ export class MapComponentComponent implements OnInit,AfterViewInit {
     // if (storage['token'] && storage['created_at'] + 3600000 > date) {
     //   console.log('in token')
     //   this.access_token = storage['token'];
-    //   this.common.twilio.connectToRoom(this.access_token, 
-    //     { name: 'test', 
-    //       audio: true, 
-    //       video: { width: 240 } 
+    //   this.common.twilio.connectToRoom(this.access_token,
+    //     { name: 'test',
+    //       audio: true,
+    //       video: { width: 240 }
     //     })
     //   return;
     // }
 
-    this.common._api.get(this.common._api.apiUrl+'/generateToken').subscribe((resp:any) => {
-      console.log(resp,'in the api part')
-      this.access_token = resp.data['token'];
-      console.log(this.access_token,'access_token')
-      // this.username = d['username'];
-      localStorage.setItem('token', JSON.stringify({
-        token: this.access_token,
-        // username:this.username,
-        created_at: date
-      }));
+    this.common._api.get(this.common._api.apiUrl + '/generateToken').subscribe((resp: any) => {
+        console.log(resp, 'in the api part')
+        this.access_token = resp.data['token'];
+        console.log(this.access_token, 'access_token')
+        // this.username = d['username'];
+        localStorage.setItem('token', JSON.stringify({
+          token: this.access_token,
+          // username:this.username,
+          created_at: date
+        }));
 
-      this.common.twilio.connectToRoom(this.access_token, 
-        { name: 'test', 
-          audio: true, 
-          video: { width: 240 } 
-        })
-    },
+        this.common.twilio.connectToRoom(this.access_token,
+          {
+            name: 'test',
+            audio: true,
+            video: {width: 240}
+          })
+      },
       error => console.log(JSON.stringify(error)));
-    
+
   }
 
 
