698761ce2bcb1c9a49f2d5f630ed086a40462406
anand <anand@tericsoft.com>
2020-09-22 00:48:44 +0530

    scope box set to 0 + friend character restricted + setup my_character and other_characters objects

--- src/app/map-component/map-component.component.ts
+++ src/app/map-component/map-component.component.ts
@@ -27,32 +27,32 @@ export class MapComponentComponent implements OnInit, AfterViewInit, OnDestroy {
   access_token: any;
   character_animation_time = 100;
 
-  last_face_towards = "right";
-
-  my_map_scope_box = {
-    top_left: {x: 0, y: 0},
-    top_right: {x: 0, y: 0},
-    bottom_left: {x: 0, y: 0},
-    bottom_right: {x: 0, y: 0}
+  my_state = {
+    id: "my_character",
+    character_width: 60,
+    character_height: 98,
+    location: {
+      x: 2760,
+      y: 760
+    },
+    character: "red_girl",
+    face_towards: "right",
+    my_map_scope_box: {
+      top_left: {x: 0, y: 0},
+      top_right: {x: 0, y: 0},
+      bottom_left: {x: 0, y: 0},
+      bottom_right: {x: 0, y: 0}
+    },
+    step_count: 30
   }
+
+  characters = [];
 
   window_width = 0;
   window_height = 0;
 
-  characters = []
-
-  my_location: any = {
-    x: 2760,
-    y: 760
-  };
-
-  character_width = 60;
-  character_height = 98;
-
   map_width = 5600;
   map_height = 5600;
-
-  step_count = 30;
 
   @ViewChild("localVideo", {static: true}) localVideo: ElementRef;
   @ViewChild("remoteVideos", {static: true}) remoteVideos: ElementRef;
@@ -62,24 +62,6 @@ export class MapComponentComponent implements OnInit, AfterViewInit, OnDestroy {
   constructor(private httpClient: HttpClient, private common: CommonService) {
   }
 
-  // ngAfterViewInit() {
-  //   this.restrictive = this.position_restrictive_items()
-  //   console.log(this.restrictive);
-  // }
-
-  // position_restrictive_items() {
-  //   const restrictive = [[7, 11],[4, 69],[27, 179],[65, 279],[86, 295],[104, 300],[125, 304],[154, 304],[179, 284],[191, 262],[213, 214],[259, 112],[240, 5],[143, 4]]
-  //   const top = 0
-  //   const left = 839.979
-  //   let new_restricitve = []
-  //   for (let index = 0; index < restrictive.length; index++) {
-  //     const element = restrictive[index];
-  //     new_restricitve.push({left:left+element[0]/2,top:top+element[1]/2})
-
-  //   }
-  //   return new_restricitve;
-
-  // }
 
   isInBox(p, box_edges) {
     // console.log(p, box_edges.top_left, box_edges.bottom_right, "is in box")
@@ -128,7 +110,6 @@ export class MapComponentComponent implements OnInit, AfterViewInit, OnDestroy {
         element.left *= 2;
         element.top *= 2;
 
-
         // let p = element['restricted_polygon']
         if (element.restricted_polygon.length > 0) {
           let new_items = []
@@ -159,7 +140,6 @@ export class MapComponentComponent implements OnInit, AfterViewInit, OnDestroy {
     let possible_conflicts = [];
     let is_within_map_boundary = false;
 
-
     let proposed_position: any = {};
     let handle_keys = [37, 38, 39, 40];
 
@@ -172,21 +152,21 @@ export class MapComponentComponent implements OnInit, AfterViewInit, OnDestroy {
     switch (key_code) {
       case 37: // left
         proposed_position = {
-          x: this.my_location.x - this.step_count,
-          y: this.my_location.y
+          x: this.my_state.location.x - this.my_state.step_count,
+          y: this.my_state.location.y
         }
 
         possible_conflicts.push({
-          x: this.my_location.x - this.step_count,
-          y: this.my_location.y
+          x: this.my_state.location.x - this.my_state.step_count,
+          y: this.my_state.location.y
         });
 
         possible_conflicts.push({
-          x: this.my_location.x - this.step_count,
-          y: this.my_location.y + this.character_height
+          x: this.my_state.location.x - this.my_state.step_count,
+          y: this.my_state.location.y + this.my_state.character_height
         });
 
-        is_within_map_boundary = this.my_location.x - this.step_count > 0
+        is_within_map_boundary = this.my_state.location.x - this.my_state.step_count > 0
         current_face_towards = "left";
 
 
@@ -196,65 +176,64 @@ export class MapComponentComponent implements OnInit, AfterViewInit, OnDestroy {
         current_face_towards = "up";
 
         proposed_position = {
-          x: this.my_location.x,
-          y: this.my_location.y - this.step_count
+          x: this.my_state.location.x,
+          y: this.my_state.location.y - this.my_state.step_count
         }
 
         possible_conflicts.push({
-          x: this.my_location.x,
-          y: this.my_location.y - this.step_count
+          x: this.my_state.location.x,
+          y: this.my_state.location.y - this.my_state.step_count
         });
 
         possible_conflicts.push({
-          x: this.my_location.x + this.character_width,
-          y: this.my_location.y - this.step_count
+          x: this.my_state.location.x + this.my_state.character_width,
+          y: this.my_state.location.y - this.my_state.step_count
         });
 
-        is_within_map_boundary = this.my_location.y - this.step_count > 0
+        is_within_map_boundary = this.my_state.location.y - this.my_state.step_count > 0
 
 
         break;
       case 39: // right
 
         proposed_position = {
-          x: this.my_location.x + this.step_count,
-          y: this.my_location.y
+          x: this.my_state.location.x + this.my_state.step_count,
+          y: this.my_state.location.y
         }
 
         possible_conflicts.push({
-          x: this.my_location.x + this.character_width + this.step_count,
-          y: this.my_location.y
+          x: this.my_state.location.x + this.my_state.character_width + this.my_state.step_count,
+          y: this.my_state.location.y
         });
 
         possible_conflicts.push({
-          x: this.my_location.x + this.character_width + this.step_count,
-          y: this.my_location.y + this.character_height
+          x: this.my_state.location.x + this.my_state.character_width + this.my_state.step_count,
+          y: this.my_state.location.y + this.my_state.character_height
         });
 
-        is_within_map_boundary = this.my_location.x + this.character_width + this.step_count < this.map_width;
+        is_within_map_boundary = this.my_state.location.x + this.my_state.character_width + this.my_state.step_count < this.map_width;
 
         current_face_towards = "right";
-
 
         break;
       case 40: // down
 
         proposed_position = {
-          x: this.my_location.x,
-          y: this.my_location.y + this.step_count
+          x: this.my_state.location.x,
+          y: this.my_state.location.y + this.my_state.step_count
         }
 
         possible_conflicts.push({
-          x: this.my_location.x,
-          y: this.my_location.y + this.character_height + this.step_count
+          x: this.my_state.location.x,
+          y: this.my_state.location.y + this.my_state.character_height + this.my_state.step_count
         });
 
         possible_conflicts.push({
-          x: this.my_location.x + this.character_width,
-          y: this.my_location.y + this.character_height + this.step_count
+          x: this.my_state.location.x + this.my_state.character_width,
+          y: this.my_state.location.y + this.my_state.character_height + this.my_state.step_count
         });
 
-        is_within_map_boundary = this.my_location.y + this.character_height + this.step_count < this.map_height;
+        is_within_map_boundary = this.my_state.location.y + this.my_state.character_height + this.my_state.step_count < this.map_height;
 
         current_face_towards = "down";
 
@@ -263,32 +242,48 @@ export class MapComponentComponent implements OnInit, AfterViewInit, OnDestroy {
         return; // exit this handler for other keys
     }
 
-    if (current_face_towards != this.last_face_towards) {
-      $('.character-wrapper img').attr('src', character_animation_base + current_face_towards + '.gif');
+    if (current_face_towards != this.my_state.face_towards) {
+      $('#my_character img').attr('src', character_animation_base + current_face_towards + '.gif');
     }
 
-    this.last_face_towards = current_face_towards;
-
+    this.my_state.face_towards = current_face_towards;
 
     if (is_within_map_boundary) {
 
       let is_proposed_position_valid = true;
+
       possible_conflicts.forEach((position) => {
+        if (!is_proposed_position_valid) {
+          return;
+        }
+
         if (this.isPointInPolygon(position.x, position.y)) {
           is_proposed_position_valid = false
           return;
         }
+        this.characters.forEach((character) => {
+          const char_box = {
+            top_left: character.location,
+            bottom_right: {
+              x: character.location.x + character.character_width,
+              y: character.location.y + character.character_height
+            },
+          }
+          if (this.isInBox(position, char_box)) {
+            is_proposed_position_valid = false;
+            return
+          }
+        });
       });
 
       if (is_proposed_position_valid) {
-        $('.character').dequeue().animate({
+        $('#my_character').dequeue().animate({
           left: proposed_position.x + 'px',
           top: proposed_position.y + 'px'
         }, this.character_animation_time);
 
-        this.my_location = proposed_position;
-
-        this.resetMapScope(this.my_location);
+        this.my_state.location = proposed_position;
+        this.resetMapScope(this.my_state.location);
 
       }
     }
@@ -296,9 +291,9 @@ export class MapComponentComponent implements OnInit, AfterViewInit, OnDestroy {
 
   resetMapScope(location) {
 
-    // console.log(this.isInBox(this.my_location, this.my_map_scope_box), "isinbox")
+    // console.log(this.isInBox(this.my_state.location, this.my_state.my_map_scope_box), "isinbox")
 
-    if (!this.isInBox(this.my_location, this.my_map_scope_box)) {
+    if (!this.isInBox(this.my_state.location, this.my_state.my_map_scope_box)) {
 
       let final_scroll = {}
 
@@ -308,8 +303,8 @@ export class MapComponentComponent implements OnInit, AfterViewInit, OnDestroy {
       let center_y = scroll_top + (this.window_height / 2);
       let center_x = scroll_left + (this.window_width / 2);
 
-      let diff_x = this.my_location.x - center_x;
-      let diff_y = this.my_location.y - center_y;
+      let diff_x = this.my_state.location.x - center_x;
+      let diff_y = this.my_state.location.y - center_y;
 
       final_scroll['scrollLeft'] = (scroll_left + diff_x) + 'px';
       final_scroll['scrollTop'] = (scroll_top + diff_y) + 'px';
@@ -319,32 +314,29 @@ export class MapComponentComponent implements OnInit, AfterViewInit, OnDestroy {
       $('html, body').dequeue().animate(final_scroll, this.character_animation_time);
     }
 
-    // if (this.my_location.y > 0.8 * current_map_scope_top) {
+    // if (this.my_state.location.y > 0.8 * current_map_scope_top) {
     //   console.log("scroll bottom")
     // }
-    // console.log("window_height", this.window_height, this.window_width, this.my_location, current_map_scope_left, current_map_scope_top)
+    // console.log("window_height", this.window_height, this.window_width, this.my_state.location, current_map_scope_left, current_map_scope_top)
   }
 
   reset_scope_box() {
-    // if my location then generate my_map_scope_box
+    // if my location then generate my_state.my_map_scope_box
     let scroll_left = $(document).scrollLeft();
     let scroll_top = $(document).scrollTop();
 
     let center_y = scroll_top + (this.window_height / 2);
     let center_x = scroll_left + (this.window_width / 2);
 
-    let box_width = 0.2 * this.window_width;
-    let box_height = 0.2 * this.window_height;
+    let box_width = 0 * this.window_width;
+    let box_height = 0 * this.window_height;
 
-    this.my_map_scope_box = {
+    this.my_state.my_map_scope_box = {
       top_left: {x: center_x - box_width, y: center_y - box_height},
       top_right: {x: center_x + box_width, y: center_y - box_height},
       bottom_left: {x: center_x - box_width, y: center_y + box_height},
       bottom_right: {x: center_x + box_width, y: center_y + box_height},
     }
-
-    console.log("this.my_map_scope_box", this.my_map_scope_box);
-
   }
 
   resize_handler(e) {
@@ -378,40 +370,45 @@ export class MapComponentComponent implements OnInit, AfterViewInit, OnDestroy {
   ngAfterViewInit() {
     this.common.twilio.localVideo = this.localVideo;
     this.common.twilio.r.push(this.remoteVideos);
-
-    this.spawn_character(this.my_location)
-  }
-
-  spawn_character(location: Location) {
-
-    let character = {
-      location: location,
-      icon: "assets/images/map_assets/character/right.gif",
-      id: Math.random()
-    }
-    this.characters.push(character);
-    // this.update_character_position("", location, 0);
-    this.render_characters(this.characters, () => {
-      // all characters rendered. update location
-      // this.update_character_position(character.id, location);
-      this.resetMapScope(this.my_location);
-
+    this.spawn_character(this.my_state, () => {
+      this.resetMapScope(this.my_state.location);
     });
+
+    let location = this.my_state.location;
+    location.x += 100;
+    location.y += 100;
+
+    let character1 = {
+      id: "character1",
+      character_width: 60,
+      character_height: 98,
+      location: {
+        x: 2760,
+        y: 760
+      },
+      character: "red_girl",
+      face_towards: "right",
+      step_count: 30
+    }
+
+    this.characters.push(character1);
+
+    this.spawn_character(character1, null);
   }
 
-  render_characters(characters, cb = null) {
+  spawn_character(character, cb) {
+    let character_animation_base = "assets/images/map_assets/character/";
     const jquery_character_wrapper = $('.character-wrapper');
-    jquery_character_wrapper.html("");
-    characters.forEach((character) => {
-      jquery_character_wrapper.append('<div style="top:' + character.location.y + 'px;left:' + character.location.x + 'px" id="' + character.id + '" class="character"> <img src="' + character.icon + '"  > </div>'
-      );
-    })
+    jquery_character_wrapper.append('<div style="top:' + character.location.y + 'px;left:' + character.location.x + 'px" id="' + character.id + '" class="character"> <img src="' + character_animation_base + character.face_towards + '.gif' + '"  > </div>');
     if (cb) {
       cb();
     }
   }
 
-  update_character_position(char_id, location: Location, animate = this.character_animation_time) {
+  update_character_position(char_id, location
+    :
+    Location, animate = this.character_animation_time
+  ) {
     console.log(location, "spawn loction")
     $('.character#' + char_id).dequeue().animate({
       left: location.x + 'px',
